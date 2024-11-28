940621c7260919868f4e16f2a7a455f6fd8c5f88
ramirososa <53145455+ramirososa@users.noreply.github.com>
2024-11-27 23:58:31 -0300

    fixes for new model

--- api.py
+++ api.py
@@ -3,12 +3,13 @@ import joblib
 import pandas as pd
 from pydantic import BaseModel
 
-rf_model = joblib.load("best_random_forest_regressor_model.pkl")
-label_encoders_guardados = joblib.load("label_encoder.pkl")
-scaler = joblib.load("scaler.pkl")
+best_model = joblib.load('best_gradient_boosting_model.pkl')
+label_encoders_guardados = joblib.load('label_encoder.pkl')
+scaler = joblib.load('scaler.pkl')
 
 app = FastAPI()
 
+#Clase pedida por la api en el mismo formato que dataset de test.
 class PredictionInput(BaseModel):
     Game_Title: str
     Year: float
@@ -18,35 +19,35 @@ class PredictionInput(BaseModel):
     Japan: float
     Rest_of_World: float
     Global: float
-    Number_of_Reviews: float
-    Summary: str
-    Wishlist: float
+    Number_of_Reviews: str
+    Wishlist: str
     Platform: str
     Genre: str
 
 def convert_to_float(value):
     if isinstance(value, str):
-        if "K" in value:
-            return float(value.replace("K", "")) * 1_000
-        elif "M" in value:
-            return float(value.replace("M", "")) * 1_000_000
-    return float(value)
+        if 'K' in value:
+            return float(value.replace('K', '')) * 1_000
+        elif 'M' in value:
+            return float(value.replace('M', '')) * 1_000_000
+        elif 'B' in value:
+            return float(value.replace('B', '')) * 1_000_000_000
+    return float(value) 
 
 def align_column_names(df):
     column_mapping = {
-        "Game_Title": "Game Title",
-        "Year": "Year",
-        "Publisher": "Publisher",
-        "North_America": "North America",
-        "Europe": "Europe",
-        "Japan": "Japan",
-        "Rest_of_World": "Rest of World",
-        "Global": "Global",
-        "Number_of_Reviews": "Number of Reviews",
-        "Summary": "Summary",
-        "Wishlist": "Wishlist",
-        "Platform": "Platform",
-        "Genre": "Genre",
+        'Game_Title': 'Game Title',
+        'Year': 'Year',
+        'Publisher': 'Publisher',
+        'North_America': 'North America',
+        'Europe': 'Europe',
+        'Japan': 'Japan',
+        'Rest_of_World': 'Rest of World',
+        'Global': 'Global',
+        'Number_of_Reviews': 'Number of Reviews',
+        'Wishlist': 'Wishlist',
+        'Platform': 'Platform',
+        'Genre': 'Genre',
     }
     df.rename(columns=column_mapping, inplace=True)
     return df
@@ -56,20 +57,12 @@ def preprocess_input(data):
 
     df = align_column_names(df)
 
-    required_columns = [
-        "Genre", "Platform", "Publisher", "Wishlist", "Number of Reviews",
-        "Year", "North America", "Europe", "Japan", "Rest of World", "Global"
-    ]
-    for col in required_columns:
-        if col not in df.columns:
-            df[col] = 0 
+    df['Wishlist'] = df['Wishlist'].apply(convert_to_float)
+    df['Number of Reviews'] = df['Number of Reviews'].apply(convert_to_float)
+    df['Publisher'] = df['Publisher'].fillna('Unknown')
+    df['Year'] = df['Year'].fillna(0)
 
-    df["Wishlist"] = df["Wishlist"].apply(convert_to_float)
-    df["Number of Reviews"] = df["Number of Reviews"].apply(convert_to_float)
-    df["Publisher"] = df["Publisher"].fillna("Unknown")
-    df["Year"] = df["Year"].fillna(0)
-
-    for col in ["Genre", "Publisher", "Platform"]:
+    for col in ['Genre', 'Publisher', 'Platform']:
         df[col] = df[col].astype(str)
         if col in label_encoders_guardados:
             df[col] = df[col].map(
@@ -78,17 +71,24 @@ def preprocess_input(data):
                 else 0
             )
 
-    expected_columns = scaler.feature_names_in_
-    df = df.reindex(columns=expected_columns)
-    print("Columnas finales para el escalador:", df.columns)
-    print("Primeras filas escaladas:", df.head())
-    df = scaler.transform(df)
+    expected_columns = best_model.feature_names_in_
+    df = df.reindex(columns=expected_columns, fill_value=0)
+
+    numeric_columns_names = [
+        'Year', 'Publisher', 'North America', 'Europe', 'Japan', 'Rest of World', 'Global',
+        'Wishlist', 'Number of Reviews', 'Platform', 'Genre', 'Rating'
+    ]
+    
+    df['Rating'] = 0
+    df[numeric_columns_names] = scaler.transform(df[numeric_columns_names])
 
     return df
 
-@app.post("/predict_rating")
+@app.post('/predict_rating')
 def predict(input_data: PredictionInput):
     processed_data = preprocess_input(input_data)
-    prediction = rf_model.predict(processed_data)
+    processed_data.drop(columns=['Rating'], inplace=True)
 
-    return {"predicted_rating": prediction[0]}
+    prediction = best_model.predict(processed_data)
+
+    return {'predicted_rating': prediction[0]}
